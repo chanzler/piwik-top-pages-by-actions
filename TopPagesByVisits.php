83b26c43325053e3fa6e907749ff936638d1e7bf
Frank Kanzler <frank@intersolve.de>
2014-10-06 23:53:18 +0200

    new

--- TopPagesByVisits.php
+++ TopPagesByVisits.php
@@ -28,7 +28,6 @@ class TopPagesByVisits extends \Piwik\Plugin
 	
 	public function getJsFiles(&$jsFiles)
 	{
-		//$jsFiles[] = 'plugins/TopPagesByVisits/javascripts/toppagesbyvisits.js';
 		$jsFiles[] = 'plugins/TopPagesByVisits/javascripts/tablesorter.js';
 	}
 	
