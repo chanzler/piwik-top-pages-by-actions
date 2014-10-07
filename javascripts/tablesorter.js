0f3734a5511dd109593455cb2d569ab780726b65
Frank Kanzler <frank@intersolve.de>
2014-10-07 20:39:46 +0200

    ss

--- javascripts/tablesorter.js
+++ javascripts/tablesorter.js
@@ -28,7 +28,7 @@ $(function() {
             		$("#idaction"+value['idaction_url']).find(".number").text(value['number']);
             	} else {
                 	$( ".position").last().remove();
-            		$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+i+"</td><td class=\"number\">"+value['number']+"</td><td>"+name+"</td><td>"+(value['time'] != null)?value['time'].split(".")[0]:"0"+":"+(value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00"+" min.</td></tr>" );
+            		$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>10</td><td class=\"number\">"+value['number']+"</td><td>"+name+"</td><td>"+(value['time'] != null)?value['time'].split(".")[0]:"0"+":"+(value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00"+" min.</td></tr>" );
             	}
             });
 
@@ -56,7 +56,7 @@ $(function() {
                 if (name == "null") {
                 	name = value['url'] 
                 }
-            	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+i+"</td><td class=\"number\">"+value['number']+"</td><td>"+name+"</td><td>"+value['time'].split(".")[0]+":"+value['time'].split(".")[1].substring(0,2)+" min.</td></tr>" );
+            	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+i+"</td><td class=\"number\">"+value['number']+"</td><td>"+name+"</td><td>"+(value['time'] != null)?value['time'].split(".")[0]:"0"+":"+(value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00"+" min.</td></tr>" );
                 i++;
             });
             $('.tpbv').each(function() {
