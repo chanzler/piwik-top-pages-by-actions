ceb54debedd377f83d4266c63296073f86109bf5
Frank Kanzler <frank@intersolve.de>
2014-10-07 22:47:56 +0200

    s

--- API.php
+++ API.php
@@ -57,7 +57,7 @@ class API extends \Piwik\Plugin\API {
 				LEFT JOIN " . \Piwik\Common::prefixTable("log_action") . " AS la2 ON llva.idaction_url = la2.idaction
 				WHERE     DATE_SUB(NOW(), INTERVAL ? MINUTE) < llva.server_time
 				AND       llva.idsite = ?
-				GROUP BY llva.idaction_url ORDER BY number desc, llva.server_time desc LIMIT 30 ";
+				GROUP BY llva.idaction_url ORDER BY number desc, llva.server_time desc LIMIT 15 ";
         
         $pages = \Piwik\Db::fetchAll($sql, array(
             $lastMinutes+($timeZoneDiff/60), $idSite 
