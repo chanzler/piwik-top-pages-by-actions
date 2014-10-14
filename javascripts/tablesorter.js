512aee1f0071733ff4f3bf321a2b1a794ef3f574
Frank Kanzler <frank@intersolve.de>
2014-10-14 21:24:59 +0200

    s

--- javascripts/tablesorter.js
+++ javascripts/tablesorter.js
@@ -37,7 +37,7 @@ $(function() {
                 	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
                 	else if (value['histNumber']-value['number'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
                 	else icon = "&nbsp;";
-                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+($( ".position").length+1)+"</td><td class=\"number\">"+value['number']+"</td><td>"+icon+"</td><td>"+name+"</td><td>"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
+                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+($( ".position").length+1)+"</td><td class=\"number\">"+value['number']+" "+icon+"</td><td>"+name+"</td><td>"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
             	}
             });
 
@@ -69,7 +69,7 @@ $(function() {
                 	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
                 	else if (value['histNumber']-value['number'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
                 	else icon = "&nbsp;";
-                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+i+"</td><td class=\"number\">"+value['number']+"</td><td>"+icon+"</td><td>"+name+"</td><td>"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
+                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+i+"</td><td class=\"number\">"+value['number']+" "+icon+"</td><td>"+name+"</td><td>"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
                 i++;
             });
             $('.tpbv').each(function() {
