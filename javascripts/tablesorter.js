511ddeb3d8f9acc36e120c4338156d4ec24e029e
Frank Kanzler <f.kanzler@telekom.de>
2014-10-15 13:49:01 +0200

    style things changed

--- javascripts/tablesorter.js
+++ javascripts/tablesorter.js
@@ -26,8 +26,8 @@ $(function() {
             $.each( data, function( index, value ){
             	if ( $("#idaction"+value['idaction_url']).length ) {
             		$("#idaction"+value['idaction_url']).find(".number").text(value['number']);
-                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
-                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
+                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
                 	else icon = "&nbsp;";
             		$("#idaction"+value['idaction_url']).find(".trend").html(icon);
             	} else {
@@ -38,10 +38,11 @@ $(function() {
                     if (name == "null") {
                     	name = value['url'] 
                     }
-                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
-                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
+                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
                 	else icon = "&nbsp;";
-                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+($( ".position").length+1)+"</td><td class=\"trend\">"+icon+"</td><td class=\"number\">"+value['number']+"</td><td>"+name+"</td><td>"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
+					rowClass = "columnodd";
+                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position "+rowClass+"\"><td>"+($( ".position").length+1)+"</td><td class=\"trend "+rowClass+"\">"+icon+"</td><td class=\"number "+rowClass+"\">"+value['number']+"</td><td class=\""+rowClass+"\">"+name+"</td><td class=\""+rowClass+"\">"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
             	}
             });
 
@@ -70,10 +71,12 @@ $(function() {
                 	name = value['url'] 
                 }
                 if(i <= numberOfEntries)
-                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
-                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 5) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
+                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
                 	else icon = "&nbsp;";
-                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position\"><td>"+i+"</td><td class=\"trend\">"+icon+"</td><td class=\"number\">"+value['number']+"</td><td>"+name+"</td><td>"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
+					rowClass = "columnodd";
+					if (i % 2 == 0) rowClass = "columneven";
+                	$( "#tpbv-tbody" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position "+rowClass+"\"><td>"+i+"</td><td class=\"trend "+rowClass+"\">"+icon+"</td><td class=\"number "+rowClass+"\">"+value['number']+"</td><td class=\""+rowClass+"\">"+name+"</td><td class=\""+rowClass+"\">"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+" min.</td></tr>" );
                 i++;
             });
             $('.tpbv').each(function() {
