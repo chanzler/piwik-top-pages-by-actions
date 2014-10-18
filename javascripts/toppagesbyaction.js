e9d6c674ef46601488899a214331f333d72b4402
Frank Kanzler <frank@intersolve.de>
2014-10-18 23:51:58 +0200

    finished

--- javascripts/toppagesbyaction.js
+++ javascripts/toppagesbyaction.js
@@ -33,36 +33,36 @@ var refreshTopPagesByActionsWidget = function (element, refreshAfterXSecs, numbe
                     if (name == "null") {
                     	name = value['url'] 
                     }
-                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
-                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
+                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 30) icon = "<img src=\"plugins/TopPagesByActions/images/doubleUpArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 30) icon = "<img src=\"plugins/TopPagesByActions/images/doubleDownArrow.png\">";
+                	else if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 20) icon = "<img src=\"plugins/TopPagesByActions/images/upArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 20) icon = "<img src=\"plugins/TopPagesByActions/images/downArrow.png\">";
                 	else icon = "&nbsp;";
 					rowClass = "columneven";
 					console.log("new item");
 					$( ".tpbv table" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position pos"+index+"\" content_id="+value['idaction_url']+"\" table_pos=\""+index+"\" style=\"position:absolute;\"><td class=\"trend "+rowClass+"\">"+icon+"</td><td class=\"number "+rowClass+"\">"+value['number']+"</td><td class=\""+rowClass+"\">"+name+"</td><td class=\""+rowClass+"\">"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+"</td></tr>" );
             	}
             });
-			//$.fn.tableSort.sortTable();
             $( ".delete").remove();
 
 			// Set each td's width
             $(".tpbv table").find('tr:first-child th').each(function() {
-            	column_widths.push($(this).outerWidth(true));
+            	column_widths.push($(this).width());
     		});
             $(".tpbv table").find('tr td').each(function() {
     			$(this).css( {
-    				minWidth: column_widths[$(this).index()],
-    				padding: 0
+    				minWidth: column_widths[$(this).index()]
     			} );
             });
 
             // Set each row's height and width
 			$(".tpbv table").find('tr').each(function() {
-				//$(this).width($(this).outerWidth(true));
+				$(this).width($(this).outerWidth(true));
 				$(this).height(settings['rowHeight']);
 			});
 
 			// Set table height and width
-            $(".tpbv table").height((data.length*settings['rowHeight'])+30);
+            $(".tpbv table").height((data.length*settings['rowHeight'])+40);
 
 			// Put all the rows back in place
             var vertical_offset = 30; // Beginning distance of rows from the table body in pixels
@@ -70,10 +70,6 @@ var refreshTopPagesByActionsWidget = function (element, refreshAfterXSecs, numbe
             	$(this).css('top', vertical_offset);
             	vertical_offset += settings['rowHeight'];
 			});
-
-//            for (j=1; j<=data.length; j++){
-//            	$(".tpbv table").find('tr.pos'+j).css({ top: 30+(j*30) }).appendTo(".tpbv table");
-//            }
 
 			//animation
 			var vertical_offset = 30; // Beginning distance of rows from the table body in pixels
@@ -107,11 +103,13 @@ var refreshTopPagesByActionsWidget = function (element, refreshAfterXSecs, numbe
                 	name = value['url'] 
                 }
                 if(i <= numberOfEntries)
-                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/uArrow.png\">";
-                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 15) icon = "<img src=\"plugins/TopPagesByActions/images/dArrow.png\">";
+                	if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 30) icon = "<img src=\"plugins/TopPagesByActions/images/doubleUpArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 30) icon = "<img src=\"plugins/TopPagesByActions/images/doubleDownArrow.png\">";
+                	else if (value['number']-value['histNumber'] > (value['histNumber'] /100) * 20) icon = "<img src=\"plugins/TopPagesByActions/images/upArrow.png\">";
+                	else if (value['histNumber']-value['number'] > (value['number'] /100) * 20) icon = "<img src=\"plugins/TopPagesByActions/images/downArrow.png\">";
                 	else icon = "&nbsp;";
 					rowClass = "columneven";
-                	$( ".tpbv table" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position pos"+index+"\" content_id="+value['idaction_url']+"\" table_pos=\""+index+"\" style=\"position:absolute;\"><td class=\"trend "+rowClass+"\">"+icon+"</td><td class=\"number "+rowClass+"\">"+value['number']+"</td><td class=\""+rowClass+"\">"+name+"</td><td class=\""+rowClass+"\">"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+"</td></tr>" );
+                	$( ".tpbv table" ).append( "<tr id=\"idaction"+value['idaction_url']+"\" class=\"position pos"+index+"\" content_id="+value['idaction_url']+"\" table_pos=\""+index+"\" style=\"position:absolute;\"><td class=\"trend "+rowClass+"\">"+icon+"</td><td class=\"number "+rowClass+"\">"+value['number']+"</td><td class=\"name "+rowClass+"\">"+name+"</td><td class=\"time "+rowClass+"\">"+((value['time'] != null)?value['time'].split(".")[0]:"0")+":"+((value['time'] != null)?value['time'].split(".")[1].substring(0,2):"00")+"</td></tr>" );
                 i++;
             });
             $('.tpbv').each(function() {
@@ -120,23 +118,21 @@ var refreshTopPagesByActionsWidget = function (element, refreshAfterXSecs, numbe
 
     			// Set each td's width
                 $(".tpbv table").find('tr:first-child th').each(function() {
-                	column_widths.push($(this).outerWidth(true));
+                	column_widths.push($(this).width());
         		});
                 $(".tpbv table").find('tr td').each(function() {
         			$(this).css( {
-        				minWidth: column_widths[$(this).index()],
-        				padding: 0
+        				minWidth: column_widths[$(this).index()]
         			} );
                 });
 
                 // Set each row's height and width
     			$(".tpbv table").find('tr').each(function() {
-    				//$(this).width($(this).outerWidth(true));
     				$(this).height(settings['rowHeight']);
     			});
 
                 // Set table height and width
-                $(".tpbv table").height((data.length*settings['rowHeight'])+30).width($(this).outerWidth());
+    			$(".tpbv table").height((data.length*settings['rowHeight'])+40);
                 
     			// Put all the rows back in place
                 var vertical_offset = 30; // Beginning distance of rows from the table body in pixels
